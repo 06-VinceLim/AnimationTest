fbce5c2f86648b03071ca43dffca12e612493546
YukimiSnow <73086228+YukimiSnow@users.noreply.github.com>
2020-11-11 17:00:36 +0800

    Yes
    Yes

---
+++ 06_Animation_Vince Lim Tze Yee/Assets/Script/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator PlayerAnimin;
+    Rigidbody Player;
+    public float speed = 5.0f;
+    bool Death;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerAnimin = GetComponent<Animator>();
+        Player = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W) && Death == false)
+        {
+            PlayerAnimin.SetBool("IsStrafe", true);
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+        if (Input.GetKeyUp(KeyCode.W) && Death == false)
+        {
+            PlayerAnimin.SetBool("IsStrafe", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Z) && Death == false)
+        {
+            PlayerAnimin.SetTrigger("TriggerAttack");
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Cube"))
+        {
+            PlayerAnimin.SetTrigger("Death");
+
+            Death = true;
+        }
+    }
+}
